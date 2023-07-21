dc200146965337fe1a51b80969c696e126b62990
José Araujo <jaraujome@gmail.com>
2023-07-21 18:30:12 -0400

    add README.cmd

---
+++ myanalitics/README.cmd
@@ -0,0 +1,14 @@
+APP MYYANALITICS (App de Analitica Web con Integracción a Moodle y genera Analisis de sentimientos de estudiantes a partir de mos mensajes de Chatt-Foro)
+
+PASOS PARA ABRIR EL APP MODO DEVELOPER
+1.- Abrir Visual estudio Code
+2.- Entrar al terminal desde el editor haciendo clic en Terminal y New.
+3.- En el prompt escribir lo siguiente .\venv\Scripts\activate
+4.- Luego ejecute lo siguiente: python manage.py runserver 4000
+
+Casos Especificos:
+
+*- Para migracciones la primera vez: python manage.py makemigrations
+*.- Cuando se alade un campo o tipo de datos: python manage.py migrate
+
+
